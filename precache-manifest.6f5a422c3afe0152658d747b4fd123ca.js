68d29afa9ce990c22a3bf040a6a92a4a74dcb754
Hardeep Singh <inventiveatsquare@gmail.com>
2020-07-29 22:34:42 +0530

    adas

---
+++ precache-manifest.6f5a422c3afe0152658d747b4fd123ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c75293e49760530bf22a",
+    "url": "./static/css/main.828b5bd2.chunk.css"
+  },
+  {
+    "revision": "c75293e49760530bf22a",
+    "url": "./static/js/main.c75293e4.chunk.js"
+  },
+  {
+    "revision": "6424dcd55a6b245ee580",
+    "url": "./static/js/1.6424dcd5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "86f3f88e6d17f6c57ad816301addedb0",
+    "url": "./index.html"
+  }
+];
